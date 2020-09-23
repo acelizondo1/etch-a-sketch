48a2bfcc92540491ab3bbfeca6bf3ca4bb6de2e6
Aaron <acelizondo11@gmail.com>
2020-09-23 14:02:32 -0500

    Created initial files for html, css, and js. Html and has basic structure. JS has initial variables described and important functions listed with their descriptions

---
+++ index.js
@@ -0,0 +1,57 @@
+//Static variables to store pixel dimensions of grid
+const gridHeight = 450;
+const gridWidth = 600;
+//Current values of cells per side
+let cellNumber = 16;
+let cellHeight;
+let cellWidth;
+
+/*Randomly generates rgb value, checks for current rgb value if set then darkens by 10%
+*returns rgb value
+*/
+let generateRgb = () => {
+
+};
+
+/*
+*/
+let highlightCell = () => {
+
+};
+
+
+/*Creates html node of one cell
+*returns node, takes no arguments
+*/
+let generateCell = () => {
+
+};
+
+/*Takes user input, convertes to int, if not a number or is outside range of 2-64 throws error
+*returns error message or int
+*/
+let parseUserInput = () => {
+
+};
+
+/*Creates grid based off cellNumber, checks if current grid is empty, if not then resets
+*this has no return value
+*/
+let generateGrid = () => {
+
+};
+
+/*
+*/
+let resetGrid = () => {
+
+};
+
+/*
+*/
+let startGrid = () => {
+
+};
+
+//Start the Grid 
+generateGrid();
